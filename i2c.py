6c6dd2af68f64682d9fb21cb7477c1df9b19ecad
Zack Dobert <zackary.dobert@gmail.com>
2022-02-08 22:16:33 -0600

    File Format

---
+++ i2c.py
@@ -0,0 +1,5 @@
+# i2c.py
+# Created by: VIEC Team (Team 6)
+
+# libraries
+    import numpy as np*
---
+++ main.py
@@ -0,0 +1,21 @@
+
+# main.py
+# Created by: VIEC Team (Team 6)
+# ECE 4336 Senior Design
+
+# Libraries
+import math
+
+
+def main():
+    print(sqrt(4))
+    # Import I2C Addresses
+
+    # Identify module by scanning RFID
+
+    # Configure module (turn on sensor)
+
+    # Read data from input module and control output module
+
+if __name__ == "__main__":
+    main()
